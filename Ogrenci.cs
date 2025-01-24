71e20b02a6e0ed46ccae1bf37dac26d395df8077
Eldiana <129501744+Elldiana@users.noreply.github.com>
2025-01-24 15:00:17 +0300

    Add files via upload

---
+++ Ogrenci.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenciUygulaması
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string ad;
+        private string soyad;
+        private string bolum;
+        private int sinif;
+        private int vizenot;
+        private int finalnot;
+        private int katilimNotu;
+        private int projeNotu;
+        private string okul;
+
+        // Yapıcı Metot
+        public Ogrenci(int _ogrenciNo, string _ad, string _soyad, string _bolum, int _sinif, int _vizenot, int _finalnot, int _katilimNotu, int _projeNotu, string _okul)
+        {
+            ogrenciNo = _ogrenciNo;
+            ad = _ad;
+            soyad = _soyad;
+            bolum = _bolum;
+            sinif = _sinif;
+            vizenot = _vizenot;
+            finalnot = _finalnot;
+            katilimNotu = _katilimNotu;
+            projeNotu = _projeNotu;
+            okul = _okul;
+        }
+
+        public void ogrenciBilgileri()
+        {
+           Console.WriteLine("Öğrenci Numarası: " + ogrenciNo);
+            Console.WriteLine("Adı: " + ad);
+            Console.WriteLine("Soyadı: " + soyad);
+            Console.WriteLine("Bölümü: " + bolum);
+            Console.WriteLine("Sınıfı: " + sinif);
+            Console.WriteLine("Vize Notu: " + vizenot);
+            Console.WriteLine("Final Notu: " + finalnot);
+            Console.WriteLine("Katılım Notu: " + katilimNotu);
+            Console.WriteLine("Proje Notu: " + projeNotu);
+            Console.WriteLine("Okulu: " + okul);
+        }
+        public double ortalamaHesapla()
+        {
+            double ortalama = (vizenot * 0.3) + (finalnot * 0.4) + (katilimNotu* 0.1)+ (projeNotu * 0.2);
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrenci okul isimi: " + okul);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ogrenciUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(123, "Ali", "Veli", "Bilgisayar Mühendisliği", 2, 70, 80, 90, 100, "Gazi Üniversitesi");
+            Console.WriteLine("Uygulammamıza Hoşgeldiniz.. Yapmak istediğiniz işlemi seçiniz: ");
+            
+            while (kontrol)
+            {
+                Menu();
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenciBilgileri();
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case "2":
+                        Console.WriteLine("Ortalama: " + ogrenci1.ortalamaHesapla());
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case "3":
+                        ogrenci1.okulGetir();
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case "4":
+                        kontrol = false;
+                        break;
+
+                }
+            }
+        }
+        static void Menu()
+        {
+            Console.WriteLine("1- Öğrenci Bilgileri");
+            Console.WriteLine("2- Ortalama Hesapla");
+            Console.WriteLine("3- Okul Getir");
+            Console.WriteLine("4- Çıkış");
+        }
+    }
+}
